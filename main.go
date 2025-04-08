03903c1cf51970429f385fd362488a295716d873
Pasha <pavlik198926@gmail.com>
2025-04-08 19:56:20 +0300

    DZ-2

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USDtoEUR = 0.92
 	const USDtoRUB = 85.85
 	const EURtoRUB = USDtoEUR * USDtoRUB
+}
+
+func getUserInput() string {
+	var num = ""
+	fmt.Scan(&num)
+	return num
+}
+
+func getCalculateResult(num int, cash_1 string, cash_2 string) float64 {
+
 }
